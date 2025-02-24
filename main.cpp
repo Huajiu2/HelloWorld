9ef1152df9e245aacb0aec7084a6c7a463897f27
jimingd <2371725140@qq.com>
2025-02-24 20:39:46 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 void sort(int arr[],int m_size)
 {
-	
+	int tmp=0;
+	for(int i=0;i<m_size-1;i++)
+		for(int j=0;j<m_size-1-i;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				tmp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=tmp;
+			}
+		}
 }
 int main()
 {
@@ -10,3 +20,5 @@ int main()
 	sort(arr,size);
 	return 0;
 }
+
+
