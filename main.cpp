665da873b576e39020f4ed4511ee4238548f62a8
jimingd <2371725140@qq.com>
2025-03-10 05:33:22 +0800

    修改sort算法为快排算法

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include<iostream>
+void sort(int arr[],int i,int j)
+{
+	int val=arr[i];
+	int l=i;
+	int r=j;
+	while(l<r)
+	{
+		while(l<r&&arr[r]>=val)
+		{
+			r--;
+		}
+		if(l<r)
+		{
+			arr[l++]=arr[r];
+		}
+		while(l<r&&arr[l]<=val)
+		{
+			l++;
+		}
+		if(l<r)
+		{
+			arr[r--]=arr[l];
+		}
+		arr[l]=val;
+		sort(arr,i,l-1);
+		sort(arr,l+1,j);
+	}
+}
 void sort(int arr[],int m_size)
 {
-	int tmp=0;
-	for(int i=0;i<m_size-1;i++)
-		for(int j=0;j<m_size-1-i;j++)
-		{
-			if(arr[j]>arr[j+1])
-			{
-				tmp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=tmp;
-			}
-		}
+	sort(arr,0,m_size);
 }
 int main()
 {
